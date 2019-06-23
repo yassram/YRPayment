bf9fcb3b5e81dc5954faa86bd7aba7cfe82fb77f
yassir ramdani <yassir.ramdani@epita.fr>
2019-06-23 09:40:30 +0200

    Initial Commit

---
+++ YRPayment/YRPayment.h
@@ -0,0 +1,19 @@
+//
+//  YRPayment.h
+//  YRPayment
+//
+//  Created by Yassir RAMDANI on 6/23/19.
+//  Copyright © 2019 Yassir RAMDANI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YRPayment.
+FOUNDATION_EXPORT double YRPaymentVersionNumber;
+
+//! Project version string for YRPayment.
+FOUNDATION_EXPORT const unsigned char YRPaymentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YRPayment/PublicHeader.h>
+
+
